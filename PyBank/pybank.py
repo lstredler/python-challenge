9255d84848170d750b1bf03ce160b3b8cb0f9b24
Streds <laurenstredler@MacBook-Pro.local>
2021-03-09 20:04:38 -0600

    update

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -11,6 +11,7 @@ profits_losses = 0
 net_total = 0 
 total_months = 0
 total_volume = 0 
+max_min = []
 
 ###calculate average
 def average(numbers):
@@ -24,9 +25,8 @@ with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
     print(csvreader)
-
+    
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader) #pop one row 
     print(f"CSV Header: {csv_header}")
@@ -36,9 +36,9 @@ with open(csvpath) as csvfile:
         total_months +=1
         total_volume += int(row[1])
         length = len(row[1])
-        average_change = (total_volume/length)
+        average_change = total_volume/length
         arr = np.array([row[0],row[1]])
-        #max_value = max(arr)
+
        
 # Print out the budget data to terminal 
 print(f"Total Months: {total_months}")
@@ -48,9 +48,12 @@ print (f"Greatest Increase in Profits: {arr}")
 print(f"Greatest Decrease in Losses: {arr}")
 
 # export data to text file int analysis folder - specify file to write  
-output_path = os.path.join("..", "output", "budget_data_csv.txt")
+output_path = os.path.join("..", "output", "Analysis")
 
 #with open(output_path, 'w') as outfile:  
     #csvwriter = csv.writer(csvfile, delimiter=',')
     #csvwriter.writerow(['Total Months:', '86'])
     #csvwriter.writerow(['Net Total:' , '$38,382,578'])
+    #csvwriter.writerow(['Average Change:' , '-2315.12'])
+    #csvwriter.writerow(['Greatest Increase in Profits', 'Feb-2012 ($1926159)'])
+    #csvwriter.writerow(['Greatest Decrease in Losses:', 'Sep-2013 ($-2196167)'])    
